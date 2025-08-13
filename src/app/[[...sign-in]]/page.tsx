1c768f666d4cdd140cd78de67ff61dc44a6152de
Pamebicho <progpame@gmail.com>
2025-08-13 00:48:41 -0400

    se crea login de la pagina

--- src/app/[[...sign-in]]/page.tsx
+++ src/app/[[...sign-in]]/page.tsx
@@ -1,13 +1,26 @@
 "use client";
 
-import * as Clerk from '@clerk/elements/common';
-import * as SignIn from '@clerk/elements/sign-in';
-import Image from 'next/image';
-
-
-
+import * as Clerk from "@clerk/elements/common";
+import * as SignIn from "@clerk/elements/sign-in";
+import { useUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const LoginPage = () => {
+
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const role = user?.publicMetadata.role;
+
+    if (role) {
+      router.push(`/${role}`);
+    }
+  }, [user, router]);
+
   return (
     <div className="h-screen flex items-center justify-center bg-blue-300">
       <SignIn.Root>
@@ -15,12 +28,17 @@ const LoginPage = () => {
           name="start"
           className="bg-white p-12 rounded-md shadow-2xl flex flex-col gap-2"
         >
-          <h1 className="text-xl font-bold flex items-center gap-2">
-            <Image src="/logo.png" alt="" width={24} height={24} />
-            Liceo Bicentenario Centro Educacional Valle Hermoso
+          <h1 className="text-xl font-bold flex flex-col items-center gap-2">
+            <Image src="/Logo LBECVH.png" alt="" width={160} height={160} />
+          </h1>
+          <h1 className="text-xl text-[#00194F] font-bold flex items-center gap-2 flex-col">
+            Liceo Bicentenario
+          </h1>
+          <h1 className="text-xl text-[#00194F] font-bold flex items-center gap-2 flex-col">
+            Centro Educacional Valle Hermoso
           </h1>
           <h2 className="text-gray-400">Inicia sesión en tu cuenta</h2>
-          <Clerk.GlobalError className="text-sm text-red-400" />
+          <Clerk.GlobalError className="text-sm text-red-400"/>
           <Clerk.Field name="identifier" className="flex flex-col gap-2">
             <Clerk.Label className="text-xs text-gray-500">
               Nombre de usuario
