24251b2730ee048636026ea57c83a8405b0283e5
Pamebicho <progpame@gmail.com>
2025-08-13 00:49:35 -0400

    cambios para los roles

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,28 @@
 import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { routeAccessMap } from './lib/settings';
+import { NextResponse } from 'next/server';
 
-const matcher = Object.keys(routeAccessMap).map(route=>({
+const matchers = Object.keys(routeAccessMap).map(route=>({
   matcher:createRouteMatcher([route]),
   allowedRoles: routeAccessMap[route]
-}))
+}));
 
-console.log(matcher)
+console.log(matchers);
 
 export default clerkMiddleware((auth, req) =>{
   //if (isProtectedRoute(req)) auth().protect()
 
   const { sessionClaims } = auth();
 
-  console.log(sessionClaims)
-})
+  const role = (sessionClaims?.metadata as {role?:string})?.role;
+
+  for (const { matcher, allowedRoles } of matchers) {
+    if (matcher(req) && !allowedRoles.includes(role!)) {
+      return NextResponse.redirect(new URL(`/${role}`, req.url));
+    }
+  }
+});
+
 
 export const config = {
   matcher: [
